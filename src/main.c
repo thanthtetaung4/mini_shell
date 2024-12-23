e52b965b70e2e9a7ba22bf486acd5873f4db050d
Lawrecess <lhshein14@gmail.com>
2024-12-23 16:13:38 +0800

    test

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: taung <taung@student.42singapore.sg>       +#+  +:+       +#+        */
+/*   By: lshein <lshein@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 16:23:27 by taung             #+#    #+#             */
-/*   Updated: 2024/12/16 18:07:03 by taung            ###   ########.fr       */
+/*   Updated: 2024/12/23 16:13:24 by lshein           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 int main()
 {
-	printf("hello world\n");
+	// printf("hello world\n");
 	char *line = readline("Enter a line >>");
 	printf("%s\n", line);
 }
