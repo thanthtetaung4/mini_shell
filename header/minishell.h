c5c5ae964f23b9c33256f5cd46979f49387c69d3
Thant Htet Aung <taung@c2r2s3.42singapore.sg>
2024-12-23 16:17:01 +0800

    ft_printf removed libft added

--- header/minishell.h
+++ header/minishell.h
@@ -1,6 +1,5 @@
 # ifndef MINI_SHELL_H
 # define MINI_SHELL_H
-# include "../ft_printf/libft/libft.h"
-# include "../ft_printf/ft_printf.h"
+# include "../libft/libft.h"
 # include <stdio.h>
 # endif
