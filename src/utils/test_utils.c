c5fe2926fec4c640633783a00ffa389eebe8b02e
Thant Htet Aung <maung.good@gmail.com>
2025-02-27 22:53:47 +0800

    heredoc interpret fix

--- src/utils/test_utils.c
+++ src/utils/test_utils.c
@@ -6,7 +6,7 @@
 /*   By: taung <taung@student.42singapore.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 06:34:35 by taung             #+#    #+#             */
-/*   Updated: 2025/02/23 01:27:42 by taung            ###   ########.fr       */
+/*   Updated: 2025/02/27 22:42:29 by taung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,7 @@ void	ft_print_args(char **args)
 	i = 0;
 	while(args[i])
 	{
-		if (i == 0)
-			printf("cmd: %s\n",args[i]);
-		else
-			printf("arg[%d]: %s\n",i,args[i]);
+		printf("arg[%d]: %s\n",i,args[i]);
 		printf("size: %ld\n", ft_strlen(args[i]));
 		i++;
 	}
